cf9969d846e224bf4ee04a8e80316fb6a4fda66a
Santiago Cobo <santiagocobo610@gmail.com>
2025-01-19 12:53:22 -0500

    Este código implementa:
    Una clase Listacon un arreglo dinámico para almacenar elementos.
    Un método Agregarpara insertar elementos en la lista.
    El método Buscarsolicitado es:
    
    Cuenta cuantas veces aparece un valor en la lista
    Muestra un mensaje apropiado con el resultado.
    Indica si el valor no fue encontrado
    
    Un metodo MostrarListapara visualizar los elementos
    Un programa principal que demuestra el funcionamiento.
    
    Al ejecutar este programa, verás:
    
    Cómo se agregan elementos a la lista
    La lista completa mostrada en pantalla
    El resultado de buscar el valor 5 (que aparece 3 veces)
    El resultado de buscar el valor 2 (que no existe en la lista)

--- Estructura de Datos/obj/Debug/net9.0/Estructura de Datos.AssemblyInfo.cs
+++ Estructura de Datos/obj/Debug/net9.0/Estructura de Datos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Estructura de Datos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93b6e48005b4ec5f92c06517ec9c736ede98c0a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("Estructura de Datos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Estructura de Datos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
